a77b3da9e101cbfb3e9157f9fe309d44311cff73
Simone Basile <Brink923@hotmail.it>
2023-05-11 18:08:54 +0200

    Tweaked canvas logic
    Tweaked logic, moved it to separate file and made some tests

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,20 +1,23 @@
 import { Col, Container } from "react-bootstrap"
 import { fabric } from "fabric";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { preventDragOffCanvas, snapControls, gridSize, addGrid, removeGrid} from "../hooks/canvasLogic";
 
 
 export const GamePage = () => {
+    const [sidebarOpen, setSideBarOpen] = useState(false);
+    const canvas = useRef<fabric.Canvas>();
 
     useEffect( () => {
-        const canvas = new fabric.Canvas("gameScreen");
-    
-          // creating grid
-          const gridSize = 50;
-          for (let i = 0; i < (800 / gridSize); i++) {
-            canvas.add(new fabric.Line([ i * gridSize, 0, i * gridSize, 800], { stroke: '#ccc', selectable: false }));
-            canvas.add(new fabric.Line([ 0, i * gridSize, 800, i * gridSize], { stroke: '#ccc', selectable: false }))
-          }
-          const block = new fabric.Rect({
+        //creating canvas
+        canvas.current = new fabric.Canvas("gameScreen", {
+            width: 800,
+            height:800
+        });
+        // creating grid
+        addGrid(canvas.current);
+
+        const block = new fabric.Rect({
             left: gridSize,
             top: gridSize,
             width: gridSize,
@@ -22,53 +25,28 @@ export const GamePage = () => {
             fill: 'red',
             originX: 'left',
             originY: 'top',
-            centeredRotation: false,
             lockScalingX: false,
             lockScalingY: false,
             lockRotation: false,
             hasControls: true,
-            cornerSize: 8,
-            hasBorders: false,
-            padding: 0
-          });
-  
-          canvas.add(block);
+            transparentCorners: false,
+            cornerColor: "#CA9534",
+            borderColor: "#CA9534",
+            cornerSize: 10,
+            hasBorders: true,
+            snapAngle:45,
+            padding: 0,
+            selectable:true
+        });
+        canvas.current.add(block);
 
-            const snapOnDrop = (options: fabric.IEvent<MouseEvent>) => {
-                const evt = options.e
-                if (evt.altKey === false) {
-                    if (options.target?.left && options.target?.top ) {
-                        options.target?.set({
-                        left: Math.round(options.target.left / gridSize) * gridSize,
-                        top: Math.round(options.target.top / gridSize) * gridSize
-                        })
-                    }
-                }
-            }
-            const snapResize = (options:fabric.IEvent<MouseEvent>) => {
-                const evt = options.e
-                    if (evt.altKey === false && options.action === "scale") {
-                        console.log(options)
-                        if (options.target && options.target.scaleX && options.target.scaleY) {
-                            options.target.set({ opacity: 1 }); 
-                            const newWidth = (Math.round(options.target.scaleX));
-                            const newHeight = (Math.round(options.target.scaleY)); 
-                            if (options.target.scaleX !== newWidth) {
-                            options.target.set({ scaleX: newWidth, scaleY: newHeight});
-                            }
-                    }
-                }
+        //activating canvas logic
+        canvas.current.on('object:moving', preventDragOffCanvas);
+        canvas.current.on("object:modified",snapControls);
             
-            }
-                canvas.on("mouse:up",snapOnDrop);
-                canvas.on("object:modified",snapResize);
-            
-          console.log("Canvas drawn!")
-          
+        console.log("Canvas drawn!")
     }, [])
-    //testing some funcionalities, I'll get back to this later
 
-    const [sidebarOpen, setSideBarOpen] = useState(false);
     const handleViewSidebar = () => {
       setSideBarOpen(!sidebarOpen);
     };
@@ -78,13 +56,13 @@ export const GamePage = () => {
         <div>
             <canvas id="gameScreen" width="800" height="800"></canvas>
             <div className={sidebarClass}>
-            <div> Sliding </div>
-            <div> Down </div>
-            <div> The hill </div>
-            <button onClick={handleViewSidebar} className="sidebar-toggle">
-                Toggle Sidebar
-            </button>
-    </div>
+                <div> Sliding </div>
+                <div> Down </div>
+                <div> The hill </div>
+                <button onClick={handleViewSidebar} className="sidebar-toggle">
+                    Toggle Sidebar
+                </button>
+            </div>
         </div>
     )
 }
---
+++ src/hooks/canvasLogic.ts
@@ -0,0 +1,83 @@
+import {fabric} from "fabric";
+
+export let gridSize = 50;
+export let gridGroup: fabric.Group;
+
+export const addGrid = (canvas: fabric.Canvas) => {
+    if (gridGroup) return;
+    const gridOptions = {
+        stroke: '#ccc',
+        strokeWidth: 1,
+    }
+    const gridLines = [];
+    if (canvas.width && canvas.height) {
+        for (let i = 1; i < (canvas.width / gridSize); i ++){
+        gridLines.push(new fabric.Line([i * gridSize, 0, i * gridSize, canvas.width], gridOptions));
+        }
+        for (let i = 1; i < (canvas.height / gridSize); i ++){
+        gridLines.push(new fabric.Line([0, i * gridSize, canvas.height, i * gridSize], gridOptions));
+        }
+        gridGroup = new fabric.Group(gridLines, {
+        selectable: false,
+        evented: false
+        })
+        gridGroup.addWithUpdate();
+        canvas.add(gridGroup);
+    }
+}
+export const removeGrid = (canvas: fabric.Canvas | undefined) => {
+    if (canvas !== undefined){
+        gridGroup && canvas.remove(gridGroup);
+        gridGroup;
+    }
+  }
+
+export const setGridSize = (newSize: number) => {
+    gridSize = newSize
+}
+
+export const snapControls = (options:fabric.IEvent<MouseEvent>) => {
+    const evt = options.e
+    if (evt.altKey === false) {
+        const action = options.transform?.action
+        if (action === "scale" || action === "scaleX" || action === "scaleY")  {
+            if (options.target && options.target.scaleX && options.target.scaleY) {
+                options.target.set({ opacity: 1 }); 
+                const newWidth = (Math.round(options.target.scaleX));
+                const newHeight = (Math.round(options.target.scaleY)); 
+
+                if (options.target.scaleX !== newWidth || options.target.scaleY !== newHeight) {
+                    if (newWidth < 1 || newHeight < 1) {
+                        options.target.set({ scaleX: 1, scaleY: 1});
+                    } else options.target.set({ scaleX: newWidth, scaleY: newHeight});
+                }
+            }
+        } else if (options.transform?.action === "drag") {
+            if (options.target?.left && options.target?.top ) {
+                options.target?.set({
+                left: Math.round(options.target.left / gridSize) * gridSize,
+                top: Math.round(options.target.top / gridSize) * gridSize
+                })
+            }
+        }
+    }
+}
+
+export const preventDragOffCanvas = (options:fabric.IEvent<MouseEvent>) =>{
+    const target = options.target
+    if (target?.getBoundingRect() && target.top && target.left && target.canvas?.height && target.canvas.width) {
+        target.setCoords();
+        if(target.getBoundingRect().top < 0 || target.getBoundingRect().left < 0) {
+            target.top = Math.max(target.top, target.top-target.getBoundingRect().top);
+            target.left = Math.max(target.left, target.left-target.getBoundingRect().left);
+        }
+        // bot-right corner
+        if(target.getBoundingRect().top+target.getBoundingRect().height  > target.canvas.height || target.getBoundingRect().left+target.getBoundingRect().width  > target.canvas.width) {
+            target.top = Math.min(target.top, target.canvas.height-target.getBoundingRect().height+target.top-target.getBoundingRect().top);
+            target.left = Math.min(target.left, target.canvas.width-target.getBoundingRect().width+target.left-target.getBoundingRect().left);
+        }
+    }
+}    
+
+
+
